099e02c0f2252501e76f1f142c42908762d72f04
Mirabror Sodikov <mirabror9545@gmail.com>
2021-05-13 00:14:22 +0500

    fix links

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const AboutScreen = () => {
   return (
@@ -6,7 +7,7 @@ const AboutScreen = () => {
       <main className="about">
         <section>
           <p>
-            <a href="./index.html">Home</a> / <a href="./about.html">About</a>
+            <Link to="/">Home</Link> / <Link to="/about">About</Link>
           </p>
           <h2>ABOUT</h2>
         </section>
