7af50624443c1b9ab0d127e4c06b82778a8e27e8
Mirabror Sodikov <mirabror9545@gmail.com>
2021-05-14 11:19:06 +0500

    search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,6 +1,48 @@
+import axios from "axios";
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
+import noimage from "../images/no-image.png";
 
-const SearchScreen = () => {
+const SearchScreen = ({ location }) => {
+  const [query, setQuery] = useState();
+  const [searchKeyword, setSearchKeyword] = useState();
+  const [isMovie, setIsMovie] = useState(true);
+  const [moviesList, setMoviesList] = useState([]);
+
+  // const [state, dispatch] = useReducer(reducer, initialState, init)
+
+  useEffect(() => {
+    const queries = new URLSearchParams(location.search);
+    const q = queries.get("q");
+    searchKeyword ? setQuery(searchKeyword) : setQuery(q);
+
+    const fetchData = async (type, query) => {
+      console.log(type);
+      const { data: moviesData } = await axios.get(
+        `https://api.themoviedb.org/3/search/${type}?api_key=2050ac8db077eda8b90b74a0f1423975&query=${query}&include_adult=false`
+      );
+
+      console.log(moviesData);
+
+      setMoviesList(moviesData.results);
+    };
+
+    if (isMovie && query) {
+      fetchData("movie", query);
+    } else if (!isMovie && query) {
+      fetchData("tv", query);
+    }
+  }, [location, isMovie, query]);
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    setQuery(searchKeyword);
+  };
+
+  const typeChangeHandler = () => {
+    setIsMovie(!isMovie);
+  };
+
   return (
     <>
       <main className="result">
@@ -15,8 +57,9 @@ const SearchScreen = () => {
                 type="text"
                 placeholder="Search by name..."
                 id="movieNameInput"
+                onChange={(e) => setSearchKeyword(e.target.value)}
               />
-              <button type="submit" id="searchButton">
+              <button type="submit" id="searchButton" onClick={searchHandler}>
                 Search
               </button>
             </form>
@@ -29,12 +72,17 @@ const SearchScreen = () => {
             <input
               type="radio"
               hidden
-              checked
+              checked={isMovie}
               id="type-movie"
               className="type-input"
               name="type"
+              readOnly
             />
-            <div id="typemovie" className="movieType">
+            <div
+              id="typemovie"
+              className="movieType"
+              onClick={typeChangeHandler}
+            >
               Movies
             </div>
           </label>
@@ -45,15 +93,47 @@ const SearchScreen = () => {
               id="type-serie"
               className="type-input"
               name="type"
+              checked={!isMovie}
+              readOnly
             />
-            <div id="typeserie" className="movieType">
+            <div
+              id="typeserie"
+              className="movieType"
+              onClick={typeChangeHandler}
+            >
               TV shows
             </div>
           </label>
         </div>
 
         <section id="resultContainer">
-          <h1>Search something</h1>
+          {query ? (
+            moviesList.length !== 0 ? (
+              <section className="figuresContainer">
+                {moviesList.map((movie) => (
+                  <Link to={`/details/${movie.id}`} key={movie.id}>
+                    <figure>
+                      <img
+                        src={
+                          movie.poster_path
+                            ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                            : noimage
+                        }
+                        alt={movie.id}
+                      />
+                      <figcaption>
+                        {movie.original_title || movie.original_name}
+                      </figcaption>
+                    </figure>
+                  </Link>
+                ))}
+              </section>
+            ) : (
+              <h1>No movies</h1>
+            )
+          ) : (
+            <h1>Search something</h1>
+          )}
         </section>
       </section>
     </>
