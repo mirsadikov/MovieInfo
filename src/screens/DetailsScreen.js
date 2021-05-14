e283b65cda77e780248ac7dfc9149ae2ea5f2e83
Mirabror Sodikov <mirabror9545@gmail.com>
2021-05-14 23:01:15 +0500

    Details Screen if Movie

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -7,7 +7,6 @@ const DetailsScreen = ({ match }) => {
   const movieID = match.params.id;
 
   const [movieInfo, setMovieInfo] = useState({});
-  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -84,49 +83,103 @@ const DetailsScreen = ({ match }) => {
         </div>
       </section>
 
-      <section className="media-wrapper">
-        <h2>Most popular</h2>
-        <div className="media container">
-          <div className="mediacontainer" id="mediacontainer">
-            <div id="novideo">
-              <h1>NO VIDEO</h1>
+      {((movieInfo.videos && movieInfo.videos.results.length > 0) ||
+        movieInfo.backdrop_path ||
+        movieInfo.poster_path) && (
+        <>
+          <section className="media-wrapper">
+            <h2>Most popular</h2>
+            <div className="media container">
+              <div className="mediacontainer" id="mediacontainer">
+                {movieInfo.videos && movieInfo.videos.results.length > 0 && (
+                  <iframe
+                    title={movieInfo.videos.results[0].name}
+                    width="600"
+                    src={`https://www.youtube-nocookie.com/embed/${movieInfo.videos.results[0].key}?controls=1`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                )}
+                {movieInfo.poster_path && (
+                  <img
+                    alt="poster"
+                    src={`https://image.tmdb.org/t/p/w500${movieInfo.poster_path}`}
+                  />
+                )}
+                {movieInfo.backdrop_path && (
+                  <img
+                    alt="backdrop"
+                    src={`https://image.tmdb.org/t/p/w500${movieInfo.backdrop_path}`}
+                  />
+                )}
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
 
-      <section className="media-wrapper">
-        <h2>Backdrops:</h2>
-        <div className="media container">
-          <div className="mediacontainer" id="media2">
-            <div id="novideo">
-              <h1>NO MEDIA</h1>
-            </div>
-          </div>
-        </div>
-      </section>
+      {movieInfo.images &&
+        movieInfo.images.backdrops.slice(1, 51).length !== 0 && (
+          <>
+            <section className="media-wrapper">
+              <h2>Backdrops:</h2>
+              <div className="media container">
+                <div className="mediacontainer" id="media2">
+                  {movieInfo.images.backdrops.slice(1, 51).map((image) => (
+                    <img
+                      alt="Backdrop"
+                      key={image.file_path}
+                      src={`https://image.tmdb.org/t/p/w500${image.file_path}`}
+                    />
+                  ))}
+                </div>
+              </div>
+            </section>
+          </>
+        )}
 
-      <section className="media-wrapper">
-        <h2>Videos:</h2>
-        <div className="media container">
-          <div className="mediacontainer" id="media3">
-            <div id="novideo">
-              <h1>NO MEDIA</h1>
+      {movieInfo.videos && movieInfo.videos.results.slice(1, 11).length !== 0 && (
+        <>
+          <section className="media-wrapper">
+            <h2>Videos:</h2>
+            <div className="media container">
+              <div className="mediacontainer" id="media3">
+                {movieInfo.videos.results.slice(1, 11).map((video) => (
+                  <iframe
+                    key={video.id}
+                    title={video.name}
+                    width="600"
+                    src={`https://www.youtube-nocookie.com/embed/${video.key}?controls=1`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  ></iframe>
+                ))}
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
 
-      <section className="media-wrapper">
-        <h2>Posters:</h2>
-        <div className="media container">
-          <div className="mediacontainer" id="media4">
-            <div id="novideo">
-              <h1>NO MEDIA</h1>
+      {movieInfo.images && movieInfo.images.posters.slice(1, 50).length !== 0 && (
+        <>
+          <section className="media-wrapper">
+            <h2>Posters:</h2>
+            <div className="media container">
+              <div className="mediacontainer" id="media4">
+                {movieInfo.images.posters.slice(1, 50).map((image) => (
+                  <img
+                    alt="Backdrop"
+                    key={image.file_path}
+                    src={`https://image.tmdb.org/t/p/w500${image.file_path}`}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </>
   );
 };
